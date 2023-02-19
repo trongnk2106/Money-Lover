e3534b2113a2d9706e43f7987e051edd1b4ff857
Nguyễn Hậu <20521300@gm.uit.edu.vn>
2023-02-20 02:17:21 +0700

    Thay đổi data được show trong wallet

--- src/Screen/Home/TransactionWallet.js
+++ src/Screen/Home/TransactionWallet.js
@@ -9,6 +9,7 @@ import {
     TouchableWithoutFeedback,
     Alert,
     FlatList,
+    SafeAreaView,
   } from "react-native";
 
 // import { AntDesign, MaterialCommunityIcons } from "react-native-vector-icons";
@@ -28,93 +29,157 @@ function  TransactionWallet({ navigation }) {
     const [ID, setID] = useState('');
     const [modalVisible, setModalVisible] = useState(false);
     const [ListData, setListData] = useState([])
+    const [ListDataFuture, setListDataFuture] =useState([])
+    const [ListDataThisMonth, setListDataThisMonth] = useState([])
+    const [ListDataLastMonth, setListDataLastMonth] = useState([])
     const [sum, setSum] = useState([])
-    const getData = useEffect(() => {
+    const [actionTriggered, setActionTriggered] = useState('')
+
+    const getDateBefore = (date)=>{
+        
+        var mm = date.slice(5,7) - 1
+        var yyyy = date.slice(0,4)
+        if(mm == 0){
+            mm = '12'
+            yyyy = yyyy - 1
+        }
+        else if(mm > 0 && mm <10){
+            mm = '0' + mm
+        }
+        var newDate = yyyy +'-' + mm
+        return newDate
+    }
+    const GetByMonth = async(mm) =>{
+        console.log(mm)
+        
+        var summ = 0
+        await db.transaction(async (tx) =>{
+            await tx.executeSql(
+              "SELECT * FROM GIAODICH ORDER BY GIAODICH.Date",
+              [],
+                async (tx, results) =>{
+                    // var sumx = []
+                    var arr = []
+                    var temp = []
+                    for (let i = 0; i < results.rows.length; i++){
+                      var a = await results.rows.item(i)
+                      if (a.Date.slice(0,7) == mm){
+                        temp.push(a)
+                      }
+                    }
+                    console.log(temp)
+                    return temp  
+                }
+            )
+            
+        }
+        )
+    }
+    const getSum = async()=>{
         // console.log(1)
         // setdb(openDatabase({ name: 'data.db', readOnly: false,createFromLocation : 1}))
-        db.transaction((tx) =>{
-                tx.executeSql(
-                    "SELECT * FROM GIAODICH",
-                    [],
-                    (tx, results) =>{
-                        var temp = []
+        
+        let dd = new Date().getDate()
+        if (dd < 10)
+        dd = '0' + dd
+        var mm = new Date().getMonth() + 1
+        if (mm < 10)
+        mm = '0' + mm
+        var yyyy = new Date().getFullYear()
+        var date = yyyy + '-' + mm + '-' + dd
+        var date1 = getDateBefore(date)
+        await db.transaction(async (tx) =>{
+            await tx.executeSql(
+              "SELECT * FROM GIAODICH ORDER BY GIAODICH.Date",
+              [],
+              (tx, results) =>{
+                        var list = []
+                        var listF = []
+                        var list0 = []
+                        var list1 = []
                         // var sumx = []
-                        var arr = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-                        
-                        var summ = [{"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0},
-                        {"THU": 0, "CHI": 0}]
-                        
+                        var arr = []
+                        var summ = [{"ID": "Now","Thu": 0, "Chi": 0}, {"ID": "Future","Thu": 0, "Chi": 0}]  
                         for (let i = 0; i < results.rows.length; i++){
-                          var a = results.rows.item(i)
-                          temp.push(a)
-                          console.log(1)
-                          if ( a.Thu == 1){
-                            summ[0]['THU'] = summ[0]['THU'] + a.Money
-                            for (let k = 0; k <12; k++){
-                                if (a.Date.slice(5,7) == arr[k])
-                                    summ[k+1]['THU'] = summ[k+1]['THU'] + a.Money
-                            }
-                          }
-                          else {
-                              summ[0]['CHI'] = summ[0]['CHI'] + a.Money
-                              for (let k = 0; k <12; k++){
-                                  // console.log(a)
-                                  if (a.Date.slice(5,7) == arr[k])
-                                      summ[k+1]['CHI'] = summ[k+1]['CHI'] + a.Money
+                            var a = results.rows.item(i)
+                            
+                            if (a.Date.slice(0, 7) == date1.slice(0,7))
+                                list1.push(a)
+                            if (a.Date <= date){
+                              if (a.Date.slice(0, 7) == date.slice(0,7))
+                                list0.push(a)  
+                              list.push(a)
+                              if (a.Money < 0){
+                                summ[0].Chi += a.Money
                               }
-                          }
-                            // sumx.push(summ)
-                            // if (results.rows.item(i).DATE.slice(5,7) == "02")
-                            //     tp[0].SUMMONEY = tp[0].SUMMONEY + results.rows.item(i).Money
-                            // console.log(results.rows.item(i).DATE.slice(5,7))
+                              else summ[0].Thu += a.Money     
+                            }
+                            listF.push(a)
+                            if (a.Money < 0){
+                                summ[1].Chi += a.Money
+                              }
+                              else summ[1].Thu += a.Money
                         }
-                        // console.log(temp, 1)
-    
-                        // setSumM(tp)
                         
                         setSum(summ)
-                        // console.log(sum)
-                        setListData(temp)
-                        // console.log(temp)
+                        // console.log(summ)
+                        setListData(list)
+                        console.log(list0)
+                        // console.log(ListData)
+                        setListDataFuture(listF)
+                        // console.log(ListDataFuture)
+                        setListDataThisMonth(list0)
+                        setListDataLastMonth(list1)
                     }
                 )
             })
-      }, []);  
+      };
+    const TinhSum = (list) =>{
+        if (list != null){
+            var summ = 0
+            for( let i = 0; i < list.length; i++){
+                summ += list[i].Money
+            }
+            return summ
+        }
+    }
+    useEffect(()=>{
+        var mm = '2023-02'
+        getSum()
+        // getDataMonth()
+        // {asd(13)}
+        // var x = GetByMonth(mm)
+        // console.log(GetByMonth(mm))
+        // getDateBefore("2023-09-19")
+        // getData()
+      }, [])  
     let ShowSum = (item) => {
-        console.log(item)
-  
+        // console.log(item)
         if (item != null){
-            return (item.CHI + item.THU)
+            return (item.Chi + item.Thu)
         }
         };
-        const getDataMonth = (summ) =>{
-            var Month = new Date().getMonth()
-            var lb = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octoberr","November", "December"]  
-            var x = [0, 0, 0, 0, 0,0,0,0,0,0,0,0]
-            if (summ != null){
-                for (i = 1; i <=12; i++){
-                    if (summ[i] != null)
-                        x[i - 1] = summ[i].CHI + summ[i].THU
-                }
-                return {
-                    labels: [lb[Month - 1], lb[Month], lb[Month + 1]],
-                    datasets: [
-                      {
-                        data: [x[Month - 1], x[Month], x[Month + 1]]
-                      }
-                    ]
-                  };
-            }
+    const getDataMonth = (list1, list2) =>{
+        var mm = new Date().getMonth() + 1
+        if (mm < 10)
+            mm = '0' + mm
+        var yyyy = new Date().getFullYear()
+        var date2 = yyyy + '-' + mm
+        var date1 = getDateBefore(date2)
+        // console.log(k1)
+            // var Month = new Date().getMonth()
+            // var lb = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octoberr","November", "December"]  
+            // var x = [0, 0, 0, 0, 0,0,0,0,0,0,0,0]
+        if (list1 != null && list1 != null){
+            return {
+                labels: [date1, date2],
+                datasets: [
+                    {
+                    data: [TinhSum(list1), TinhSum(list2)]
+                    }
+                ]
+                };
+        }
         }
         const ShowTHU = (ListData) =>{
             // console.log(123)
@@ -148,6 +213,29 @@ function  TransactionWallet({ navigation }) {
             }
             
         }
+        const Show3GD = (ListData) =>{
+
+            if (ListData != null){
+                // console.log(ListData)
+                var data = []
+                var k = 0
+                for (let  i = ListData.length - 1; i >= 0; i-- ){
+                    var x = {"Date": ListData[i].Date, "Money": ListData[i].Money, "Category": ListData[i].Category}
+                    data.push(x)
+                    k = k + 1 
+                    if ( k == 3)
+                      break
+                }
+                return(
+                    <View>
+                    <DisplayRow data = {data[0]} />
+                    <DisplayRow data = {data[1]}/>
+                    <DisplayRow data = {data[2]}/>
+                </View>
+                )
+            }
+            
+        }
         const ShowCHI = (ListData) =>{
             // console.log(123)
             if (ListData != null){
@@ -174,6 +262,32 @@ function  TransactionWallet({ navigation }) {
                 )
             }
             
+        }
+        let listItemView = (item) => {
+            return (
+              <View 
+                key={item.user_id}
+                style={{backgroundColor: 'white', padding: 20}}>
+                <DisplayRow data = {item} />
+              </View>
+            );
+          };
+        const show =()=>{
+            return(
+                <SafeAreaView style={{flex: 1}}>
+                <View style={{flex: 1}}>
+                    <View style={{flex: 1}}>
+                        <FlatList
+                            data={ListData}
+                            // ItemSeparatorComponent={listViewItemSeparator}
+                            keyExtractor={(item, index) => index.toString()}
+                            renderItem={({item}) =>listItemView(item)}
+                    />
+            </View>
+            
+          </View>
+        </SafeAreaView>
+            )
         }   
     return (
         <View style={styles.container}>
@@ -181,7 +295,7 @@ function  TransactionWallet({ navigation }) {
                 {/* summary container */}
                 <View style={[styles.homeDiv, styles.homeSummary]}>
                 <Text style={styles.summHeading}>Số dư</Text>
-                <Text style={{fontSize: 14,fontFamily: "Poppins",textAlign:"center"}}>{ShowSum(sum[0])} </Text>
+                <Text style={{fontSize: 14,fontFamily: "Poppins",textAlign:"center"}}>{TinhSum(ListData)} VNĐ </Text>
                 {/* <View>
                    
                     <FlatList
@@ -199,7 +313,7 @@ function  TransactionWallet({ navigation }) {
                 {/* chart container */}
                 <View style={[styles.homeDiv]}>
                 <BarChart
-                    data={getDataMonth(sum)}
+                    data={getDataMonth(ListDataLastMonth,ListDataThisMonth)}
                     // {{
                     // labels: ["January", "February", "March", "April", "May", "June"],
                     // datasets: [
@@ -210,7 +324,10 @@ function  TransactionWallet({ navigation }) {
                     // }}
                     width={Dimensions.get("window").width - 30}
                     height={220}
-                    yAxisLabel={"Rs"}
+                    fromZero = {1}
+                    yAxisLabel={"VNĐ"}
+                    showBarTops = {1}
+                    showValuesOnTopOfBars = {1}
                     chartConfig={{
                     backgroundColor: "#1cc910",
                     backgroundGradientFrom: "#eff3ff",
@@ -232,35 +349,70 @@ function  TransactionWallet({ navigation }) {
 
                 <View style={styles.row}>
                     <Text style={styles.rowHeading}>Recent Income</Text>
-                    <TouchableOpacity>
+                    {/* <TouchableOpacity>
                     <Text style={styles.summText}>View All</Text>
+                    </TouchableOpacity> */}
+                    <TouchableOpacity
+                        style={styles.selectBtn}
+                        onPress={() => {
+                            setModalVisible(true);
+                            setActionTriggered('ShowAll');}}
+                    >
+
+                        <Text style={styles.selectTxt}>
+                            View all
+                        </Text>
                     </TouchableOpacity>
                 </View>
-                {ShowTHU(ListData)}
+                {Show3GD(ListData)}
                 {/* <View>
                     <DisplayRow />
                     <DisplayRow />
                     <DisplayRow />
                 </View> */}
                 </View>
-                {/* end of spending container */}
-                {/* earning container */}
-                <View style={[styles.homeDiv, styles.homeSpending]}>
-                {/* header row */}
-                <View style={styles.row}>
+                <Modal
+                        visible={modalVisible}
+                        onRequestClose={() => setModalVisible(!modalVisible)}
+                        transparent={true}
+                    >
+                        {actionTriggered === 'ShowAll' ?
+
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                            <Text style={styles.selectHeading}>
+                                BÁO CÁO GIAO DỊCH THÁNG
+                            </Text>
+                            <View style={styles.divider}></View>
+
+                            <View style={{height: 400}}>
+
+                                    {show()}
+                                    <TouchableOpacity onPress = {() => {
+                                        setModalVisible(!modalVisible)
+                                        
+                                    }}>
+                                    <Text style={{fontFamily:"PoppinsMedium", fontSize:14}}>Done</Text>
+                                    </TouchableOpacity>
+                                    
+
+                                
+                            </View>
+
+
+
+                            </View> 
+                        </View> : null}
+                    </Modal>
+                {/* <View style={[styles.homeDiv, styles.homeSpending]}> */}
+                {/* <View style={styles.row}>
                     <Text style={styles.rowHeading}>Recent Expenses</Text>
                     <TouchableOpacity>
                     <Text style={styles.summText}>View All</Text>
                     </TouchableOpacity>
-                </View>
-                {/* contents row */}
-                {ShowCHI(ListData)}
-                {/* <View>
-                    <DisplayRow />
-                    <DisplayRow />
-                    <DisplayRow />
                 </View> */}
-                </View>
+                {/* {ShowCHI(ListData)} */}
+                {/* </View> */}
                 {/* end of earning container      */}
             </ScrollView>
             
@@ -328,6 +480,19 @@ homeEarnings: {
     padding: 10,
     borderRadius: 8,
 },
+selectHeading: {
+    textAlign: "center",
+    fontFamily: "PoppinsBold",
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+centeredView: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    alignContent: "center",
+    backgroundColor: "rgba(0,0,0, 0.3)",
+  },
 row: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -339,6 +504,13 @@ rowHeading: {
     fontFamily: "PoppinsBold",
     fontWeight: "bold"
 },
+modalView: {
+    backgroundColor: "#fff",
+    width: Dimensions.get("screen").width,
+    padding: 20,
+    borderTopRightRadius: 6,
+    borderTopLeftRadius: 6,
+  },
 footer: {
     flex: 0,
     justifyContent: "flex-end",
